4bcf17f40c6d4d5cd2c7c7a014460478664171b6
Sayantan0007 <122517431+Sayantan0007@users.noreply.github.com>
2025-10-08 00:38:43 +0530

    up

--- src/components/HomePage/HomeDetails/HomeDetails.jsx
+++ src/components/HomePage/HomeDetails/HomeDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../../Loader/Loader";
-import "./HomeDetails.css";
+import "./homeDetails.css";
 import { useDispatch, useSelector } from "react-redux";
 import StarRating from "../../StarRating/StarRating";
 
